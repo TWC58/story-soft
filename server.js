191dd71f61c05a9dba6d6dc2bafda727f91a7d4e
TWC58 <47580948+TWC58@users.noreply.github.com>
2022-03-26 16:09:12 -0400

    show data

--- server.js
+++ server.js
@@ -6,9 +6,21 @@ const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 8080;
+//const routes = require('./routes/api');
 
 //Log HTTP Requests
 app.use(morgan('tiny'));
+
+//Connect to mongodb
+//mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/story-soft', {
+  //  useNewUrlParser: true,
+    //useUnifiedTopology:true
+//});
+
+//Log connection
+//mongoose.connection.on('connected', () => {
+  //  console.log('Mongoose is connected...');
+//});
 
 //testing
 app.get('/', (req, res) => {
