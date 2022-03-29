11ff3f9e63e627884811c3f7058737dfb19fd014
Harry <harrykim900@yahoo.com>
2022-03-29 12:59:33 -0400

    Added tag schema

---
+++ models/tag-model.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const ObjectId = Schema.Types.ObjectId
+
+const TagSchema = new Schema(
+    {
+        _id: { type: ObjectId, required: true },
+        name: { type: String, required: true },
+        posts: { type: [ObjectId], required: true }
+    },
+    { timestamps: true },
+)
+
+module.exports = mongoose.model('Tag', TagSchema)
