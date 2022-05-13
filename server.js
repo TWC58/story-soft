ec3436bd55a0989caae67cd200e0232c59efacbd
Aaron Berkman <arberkman@cs.stonybrook.edu>
2022-05-13 17:23:58 -0400

    increased minimum file size allowed for polotno uploads

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 //JSON body parsing
-app.use(express.json());
+app.use(express.json({limit: '50mb'}));
 
 //Log HTTP Requests
 app.use(morgan('tiny'));
